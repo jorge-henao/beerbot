c699f921503681ad186a04b016b71fc42306233f
Jorge Henao <jhenao@intergrupo.com>
2017-11-09 11:48:18 -0500

    Brewerdb information integrated in bot messages

--- Dialogs/ReceiveAttachmentDialog.cs
+++ Dialogs/ReceiveAttachmentDialog.cs
@@ -9,6 +9,7 @@
     using Microsoft.Bot.Builder.Dialogs;
     using Microsoft.Bot.Connector;
     using beerbot.BeerData;
+    using System.Collections.Generic;
 
     [Serializable]
     internal class ReceiveAttachmentDialog : IDialog<object>
@@ -28,7 +29,7 @@
                 using (HttpClient httpClient = new HttpClient())
                 {
                     // Skype & MS Teams attachment URLs are secured by a JwtToken, so we need to pass the token from our bot.
-                    if ((message.ChannelId.Equals("skype", StringComparison.InvariantCultureIgnoreCase) || message.ChannelId.Equals("msteams", StringComparison.InvariantCultureIgnoreCase)) 
+                    if ((message.ChannelId.Equals("skype", StringComparison.InvariantCultureIgnoreCase) || message.ChannelId.Equals("msteams", StringComparison.InvariantCultureIgnoreCase))
                         && new Uri(attachment.ContentUrl).Host.EndsWith("skype.com"))
                     {
                         var token = await new MicrosoftAppCredentials().GetTokenAsync();
@@ -43,9 +44,14 @@
                     var beerBrand = await beerVision.IdentifyBeer(picBytes);
 
                     if (beerBrand != null)
-                        await context.PostAsync($"Parece que se trata de una cerveza {beerBrand}");
+                    {
+                        //await context.PostAsync($"Parece que se trata de una cerveza {beerBrand}");
+                        await ShowBeerData(beerBrand, context, argument);
+                    }
                     else
+                    {
                         await context.PostAsync("lo siento , no encuentro ninguna cerveza en la imágen");
+                    }
 
                     //await context.PostAsync($"Attachment of {attachment.ContentType} type and size of {contentLenghtBytes} bytes received.");
                 }
@@ -57,5 +63,53 @@
 
             context.Wait(this.MessageReceivedAsync);
         }
+
+        private async Task ShowBeerData(string beerName, IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            BreweryDB breweryDB = new BreweryDB();
+            var searchResults = await breweryDB.SearchBeer(beerName);
+            if (searchResults.data != null & searchResults.data.Count > 0)
+            {
+                var beerData = searchResults.data[0];
+
+                if (beerData.labels != null)
+                {
+                    var pic = await GetInternetAttachment(beerData.labels.medium);
+                    var replyMessage = context.MakeMessage();
+                    replyMessage.Attachments = new List<Attachment> { pic };
+                    await context.PostAsync(replyMessage);
+                }
+
+                var beerInfoString = await GetBeerDataString(beerData);
+                await context.PostAsync(beerInfoString);
+            }
+        }
+
+        private async Task<string> GetBeerDataString(BeerData beerData)
+        {
+            string result = string.Empty;
+
+            var beerStyle = beerData.style != null ? beerData.style.shortName : string.Empty;
+            var beerIBU = beerData.ibu != null ? $" - {beerData.ibu} IBU" : string.Empty;
+
+            result = $"{beerData.nameDisplay}"
+                + $"{Environment.NewLine} {beerStyle}"
+                + $"{Environment.NewLine} {beerData.abv}% ABV {beerIBU}"
+                + $"{Environment.NewLine}{Environment.NewLine} {beerData.description}";
+
+
+            return result;
+        }
+
+        private async Task<Attachment> GetInternetAttachment(string picUrl)
+        {
+            return new Attachment
+            {
+                Name = "BotFrameworkOverview.png",
+                ContentType = "image/png",
+                ContentUrl = picUrl
+            };
+        }
+
     }
 }
